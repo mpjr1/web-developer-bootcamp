e96e095d79283373b1edab772c8599b3dc6a0118
mp_jr1 <mapg.jr@gmail.com>
2016-10-22 03:41:03 +0000

    Stalker JavaScript Excercise, correct code

---
+++ stalker.js
@@ -0,0 +1,7 @@
+// JavaScript File
+var firstName = prompt("What is your First Name?");
+var lastName = prompt("What is your Last Name?");
+var age = prompt("How old are you?");
+
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("and you are " + age + " years old.");
