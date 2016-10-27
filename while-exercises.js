ea68533c0e8dfe277f3cb73657343047835d1b52
mp_jr1 <mapg.jr@gmail.com>
2016-10-27 23:09:53 +0000

    While Javascript exercises completed

---
+++ while-exercises.js
@@ -0,0 +1,36 @@
+// JavaScript File
+console.log("Print all numbers from -10 to 19")
+
+var num = -10
+while(num <= 19)
+{ console.log(num);
+num++;}
+
+console.log("Print all even numbers from 10 to 40")
+
+var num = 10
+while(num <= 40)
+{ console.log(num);
+    num+=2;}
+    
+    // while(num <= 40)
+    // { if(num % 2 === 0)
+    //     {console.log(num);}
+    //     num++;}
+    
+console.log("Print all odd numbers between 300 and 333")
+
+var num = 300
+while(num <= 333)
+{ if(num % 2 !== 0)
+    {console.log(num);}
+     num++;}
+     
+
+console.log("Print all numbers divisible by 5 and 3 from 5 to 50")
+
+var num = 5
+while(num <= 50)
+{ if(num % 5 === 0 && num % 3 === 0)
+    {console.log(num);}
+     num++;}
