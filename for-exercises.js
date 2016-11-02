f0ad918608b4e62b9757950828ccc3238b56be90
mp_jr1 <mapg.jr@gmail.com>
2016-11-02 22:47:30 +0000

    For Loop Excersises complete

---
+++ for-exercises.js
@@ -0,0 +1,53 @@
+// JavaScript File
+console.log("Print all numbers from -10 to 19")
+for(i = -10; i <= 19; i++){
+    console.log(i);
+}
+// var num = -10
+// while(num <= 19)
+// { console.log(num);
+// num++;}
+
+console.log("Print all even numbers from 10 to 40")
+for(i = 10; i <=40; i++){
+    console.log(i);
+}
+
+// var num = 10
+// while(num <= 40)
+// { console.log(num);
+//     num+=2;}
+    
+//     // while(num <= 40)
+//     // { if(num % 2 === 0)
+//     //     {console.log(num);}
+//     //     num++;}
+    
+console.log("Print all odd numbers between 300 and 333")
+for(i = 300; i <=333; i++){
+    if(i%2 !== 0)
+        {console.log(i);}
+}
+
+// var num = 300
+// while(num <= 333)
+// { if(num % 2 !== 0)
+//     {console.log(num);}
+//      num++;}
+     
+
+console.log("Print all numbers divisible by 5 and 3 from 5 to 50")
+for(i=5; i <= 50; i++)
+    {
+        if(i%5 === 0 && i % 3 === 0)
+            {
+                console.log(i);
+                
+            }   
+    }
+
+// var num = 5
+// while(num <= 50)
+// { if(num % 5 === 0 && num % 3 === 0)
+//     {console.log(num);}
+//      num++;}
