1580e54ece8bff87238a6932cde3c147d861b542
mp_jr1 <mapg.jr@gmail.com>
2016-12-16 20:15:24 +0000

    jQuery Todo list App Part7 completed

--- jQuery Todo-list App/assets/js/todo.js
+++ jQuery Todo-list App/assets/js/todo.js
@@ -15,8 +15,9 @@ $("input[type='text']").keypress(function(event){
     if(event.which === 13){
         //grabbing new todo text from input
         var todoText = $(this).val();
+        //cleat input textbox
         $(this).val("");
         //create new li and add to ul
-        $("ul").append("<li><span>X </span>" + todoText + "</li>");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span>" + todoText + "</li>");
     }
 });
